5866eb5ce4e5c89dc6c93f73eca3e25915a86862
Sapna Gangwar <sapna.iaf@gmail.com>
2021-07-29 14:05:52 -0500

    added hello world

--- PracticeGIT2021/src/com/sapna/July29/helloWorld.java
+++ PracticeGIT2021/src/com/sapna/July29/helloWorld.java
@@ -3,7 +3,8 @@ package com.sapna.July29;
 public class helloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("practice Git");
+		System.out.println("practice Git1");
+		System.out.println("practice Git2");
 		
 	}
 
