f76a94682aaa6ee13414df258fc610481d496135
Sapna Gangwar <sapna.iaf@gmail.com>
2021-07-29 15:12:25 -0500

    testing mergetool

--- PracticeGIT2021/src/com/sapna/July29/helloWorld.java
+++ PracticeGIT2021/src/com/sapna/July29/helloWorld.java
@@ -3,7 +3,7 @@ package com.sapna.July29;
 public class helloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("practice Git1");
+		System.out.println("practice GitNew");
 		System.out.println("practice Git2");
 		
 	}
